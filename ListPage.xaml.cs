67d996a1db466f251b77d768fb27981618e54d24
gramatudorionut2000 <grama.tudorionut@gmail.com>
2022-12-18 10:42:20 +0200

    Laborator9

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -3,10 +3,10 @@ namespace Grama_Tudor_Ionut_Lab7;
 
 public partial class ListPage : ContentPage
 {
-	public ListPage()
-	{
-		InitializeComponent();
-	}
+    public ListPage()
+    {
+        InitializeComponent();
+    }
 
     async void OnSaveButtonClicked(object sender, EventArgs e)
     {
@@ -21,5 +21,33 @@ public partial class ListPage : ContentPage
         await App.Database.DeleteShopListAsync(slist);
         await Navigation.PopAsync();
     }
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)
+       this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
 
+
+
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
+
+    async void OnDeleteProductButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+        var product = listView.SelectedItem as Product;
+        await App.Database.DeleteListProductAsync(slist, product);
+        await Navigation.PopAsync();
+
+
+    }
 }
